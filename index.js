b8cdbfccb22dd0810aa75c97f27748ca72cf5a2b
pavan6cs <bhavanas904@gmail.com>
2022-09-29 12:55:31 +0530

    added styling

--- index.js
+++ index.js
@@ -1,51 +1,39 @@
 const initialPrice = document.querySelector("#initial-price");
 const currentPrice = document.querySelector("#current-price");
-const stocksQuantity = document.querySelector('#stocks-quantity');
-const showBtn = document.querySelector('#show-button');
-const outputText = document.querySelector('#output-text');
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const showBtn = document.querySelector("#show-button");
+const outputText = document.querySelector("#output-text");
 
-showBtn.addEventListener("click", submitHandler)
+outputText.style.display = "none";
+showBtn.addEventListener("click", submitHandler);
 
-function submitHandler(){
-
+function submitHandler() {
   var ip = Number(initialPrice.value);
   var qty = Number(stocksQuantity.value);
   var curr = Number(currentPrice.value);
-   
-  if(!ip && !qty && !curr){
-    alert("please fill out all fields");
-  }
+
   calculateProfitAndLoss(ip, qty, curr);
 }
 
-function showOutput(msg){
-  outputText.innerText = msg;
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    // loss logic here
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+    outputText.style.display = "block";
+    outputText.style.background = "#F32424";
+    outputText.innerText = `Hey the loss is ${loss} and the percent is ${lossPercentage}%`;
+  } else if (current > initial) {
+    // profit logic here
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+    outputText.style.display = "block";
+    outputText.style.background = "#3EC70B";
+    outputText.innerText = `Hey the profit is ${profit} and the percent is ${profitPercentage}%`;
+  } else {
+    // the rest of logic
+    outputText.style.display = "block";
+    outputText.style.background = "#FCF8E8";
+    outputText.innerText = `No pain no gain and no gain no pain`;
+  }
 }
-  
-function calculateProfitAndLoss(initial, quantity, current){
-    if(initial > current){
-      // loss logic here
-      var loss = (initial - current) * quantity;
-      var lossPercentage = (loss/initial) * 100;
-     showOutput( `Hey the loss is ${loss} and the percent is ${lossPercentage}%`);
-    } else if(current > initial){
-          // profit logic here
-          var profit = (current - initial) * quantity;
-          var profitPercentage = ( profit / initial) * 100;
-            showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}%`) 
-    }else{
-        // the rest of logic
-      showOutput(`No pain no gain and no gain no pain`)
-    }
-}
-
-calculateProfitAndLoss(10,10,10);
-calculateProfitAndLoss(10,10,100);
-calculateProfitAndLoss(100,10,10);
-
-
-
-
-
-
-
